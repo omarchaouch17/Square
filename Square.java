16703231aa0741f3d3851a71e8c61d7b4b2b8dc9
omarchaouch17 <147279623+omarchaouch17@users.noreply.github.com>
2023-10-21 16:14:21 +0200

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,10 @@
+public class Square{
+   public static void main(String[]args){
+     int zahl = Integer.parseInt(args[0]);
+     int quadrat = zahl * zahl;
+     System.out.println(zahl+"square="+quadrat);
+
+   }
+
+
+   }
